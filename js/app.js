268e566af52c2c6bff04aadd3af9edeb63506a10
JJK70 <jjk701@gmail.com>
2022-09-09 13:52:09 -0400

    added variables for board, winner, turn

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log('sanity check')
+/*-------------------------------- Constants --------------------------------*/
+
+
+/*-------------------------------- Variables --------------------------------*/
+let board. turn, winner
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+// 1a) Create variable named `board` to represent the state of the squares on
+// the board.
+
+// 1b) Create variable named `turn` to track whose turn it is.
+
+// 1c) Create variable named `winner` to represent if anyone has won yet, or
+// if a tie has occurred.
